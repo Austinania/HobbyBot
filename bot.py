526fcf664bb51515b9b9606d45e483fb3683bcc3
Austinania <58968307+Austinania@users.noreply.github.com>
2022-10-04 02:41:05 -0500

    Update
    Changed to using bot.command properly
    Made multi-text responses work
    Added a status

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 import botresponses
 
 INTENTS = discord.Intents(messages = True, message_content = True, guilds = True, members = True, guild_typing = True, typing = True)
-PREFIX = 'h!'  # commands.when_mentioned_or('h!')
+PREFIX1 = 'h!'
+PREFIX = commands.when_mentioned_or(PREFIX1)
 
 async def send_message(message, user_message, is_private):
     try:
@@ -19,27 +20,35 @@ async def send_message(message, user_message, is_private):
 def run_discord_bot():
     load_dotenv()
     TOKEN = os.getenv("DISCORD_TOKEN")
-    client = discord.Client(intents=INTENTS)
+    bot = commands.Bot(intents=INTENTS, command_prefix=PREFIX)
+    activity_string = 'on {} servers.'.format(len(bot.guilds)+1)
     
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is running.')
-        
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+        print(f'{bot.user} is running.')
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity_string + ' | ' + PREFIX1))
     
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-        # if contains global PREFIX, interprets message, otherwise ignores
-        if user_message[0:len(PREFIX)] == PREFIX:
-            is_prefixed = True
-            user_message = user_message[len(PREFIX):]
-            await send_message(message, user_message, is_prefixed=is_prefixed)
-            print(f'Command initiated by {username} ({channel})')
-        else:
-            pass
-            
-    client.run(TOKEN)
+    @bot.command(
+    name="ping",
+    help="Help descriptor here",
+    brief="Brief descriptor",
+    )
+    async def name_of_command_unless_name_is_seperately_declared(ctx):
+        await ctx.send('Pong!')
+
+    @bot.command(
+    help="Generates a roster for JJBA-ASBR game",
+    brief="Roster for JJBA-ASBR game",
+    )
+    async def JJTeam(ctx):
+        jjteam = botresponses.bot_respond('jjbaasbr').split(',')
+        await ctx.send(f'{jjteam[0]}')
+        await ctx.send(f'{jjteam[1]}')
+        await ctx.send(f'{jjteam[2]}')
+        await ctx.send(f'{jjteam[3]}')
+        await ctx.send(f'{jjteam[4]}')
+        await ctx.send(f'{jjteam[5]}')
+        await ctx.send(f'')
+
+
+    bot.run(TOKEN)
